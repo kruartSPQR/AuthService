08a4671b06bb22e2d2d44a02fe5a61435d3b5ac9
lyber_spqr <artuom0405@gmail.com>
2025-08-27 20:37:40 +0300

    fixed properties

--- src/main/java/com/innowise/authenticationService/controller/AuthController.java
+++ src/main/java/com/innowise/authenticationService/controller/AuthController.java
@@ -9,25 +9,23 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("api/v1/auth")
 public class AuthController {
 
     private UserCredentialsService userCredentialsService;
     private TokenService tokenService;
 
-    @PostMapping("/api/v1/signup")
+    @PostMapping("/signup")
     @ResponseBody
     public ResponseEntity<UserCredentialsDto> saveUserCredentials(@Valid @RequestBody UserCredentialsDto userCredentialsDto) {
         return new ResponseEntity<>(userCredentialsService.createUser(userCredentialsDto), HttpStatus.CREATED);
     }
     
-    @PostMapping("/api/v1/signin")
+    @PostMapping("/signin")
     public ResponseEntity<TokenResponse> signIn(@Valid @RequestBody TokenRequest tokenRequest) {
         return new ResponseEntity<>(tokenService.authenticate(tokenRequest), HttpStatus.OK);
 
--- src/main/java/com/innowise/authenticationService/controller/TokenController.java
+++ src/main/java/com/innowise/authenticationService/controller/TokenController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/token")
+@RequestMapping("api/v1/auth/token")
 @AllArgsConstructor
 public class TokenController {
 
